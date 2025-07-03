e390ef71939e58992585e16117d313ca08a8f136
Komola Khidirova <komolakhidirova@gmail.com>
2025-07-03 12:06:12 +0500

    auth

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import { Button } from '@/components/ui/button'
+import { UserButton } from '@clerk/nextjs'
 
 export default function Home() {
 	return (
 		<div>
 			<h2>Hello</h2>
 			<Button>Click</Button>
+			<UserButton />
 		</div>
 	)
 }
